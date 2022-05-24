85a58d5168cf7bb322f8701ec736f104a3b19087
SethJustus <seth.barrere@charthouseusa.com>
2022-05-24 11:41:31 -0400

    Unity Files

--- YHG_UNITY/Assets/Scripts/Player_Movement.cs
+++ YHG_UNITY/Assets/Scripts/Player_Movement.cs
@@ -6,21 +6,44 @@ public class Player_Movement : MonoBehaviour
 {
     public float Speed;
     public float Drag;
+    public float JumpHeight;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
+    void Jump()
+    {
+        bool jumped = false;
+        if(jumped == false)
+        {
+            rb.AddForce(transform.up * JumpHeight, ForceMode2D.Impulse);
+            jumped = true;
+        }
+        
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
+        #region Player Input
         float walk = Input.GetAxis("Horizontal")*Speed;
         float jump = Input.GetAxis("Jump");
+        if(Input.GetAxis("Jump") == 1)
+        {
+            Jump();
+        }
+        #endregion
+
+        #region Move the player
+
+        //Makes a new Vector3 by which to move the player by.
         Vector3 horizontalMovement = new Vector3(walk,0,0);
+        //Adds force to the player
         rb.AddForce(horizontalMovement, ForceMode2D.Force);
+        //Adds drag to the player
         rb.drag = Drag;
-        //transform.Translate(horizontalMovement);
+        #endregion
     }
 }
+
