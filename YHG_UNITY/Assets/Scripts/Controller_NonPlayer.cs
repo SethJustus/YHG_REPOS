1d7946827a8fc998d2cfbbce20803e41d53a3d63
SethJustus <105650714+SethJustus@users.noreply.github.com>
2022-06-07 14:44:19 -0400

    Edited settings, and worked on dialogue system.

--- YHG_UNITY/Assets/Scripts/Controller_NonPlayer.cs
+++ YHG_UNITY/Assets/Scripts/Controller_NonPlayer.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Controller_NonPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Dialogue dialogue;
+    public bool talking;
+    private Canvas canvas;
+    private Queue<string> sentences;
+    
     void Start()
     {
-        
+        canvas = GetComponentInChildren<Canvas>();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void StartDialogue(Dialogue dialogue)
     {
         
+        // NPC.talking = true;
+        // playerState.talking = true;
+        // display.SetActive(true);
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        
+        DisplayNextSentence();   
+        //displaying = true;     
+    }
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        //dialogueText.text = sentence;
+        Debug.Log(sentence);
+    }
+    public void EndDialogue()
+    {
+        // playerState.talking = false;
+        // NPC.talking = false;
+        // display.SetActive(false);
+        // displaying = false;
     }
 }
